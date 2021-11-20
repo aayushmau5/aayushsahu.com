5e38c98848f6f8b916f03b8d5debeea92fc5fee2
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-11-20 14:21:57 +0530

    add mobile nav(mvp)

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,33 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 import styles from "../styles/Nav.module.css";
 import NavLink from "./NavLink";
 
 export default function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
   const router = useRouter();
+
+  function changeMenuOpenState() {
+    setMenuOpen((prev) => !prev);
+  }
 
   return (
     <div className={styles.container}>
+      <div className={styles.mobileNavContainer}>
+        <HamburgerMenuButton onClick={changeMenuOpenState} />
+        {menuOpen ? <HamburgerMenu router={router} /> : null}
+      </div>
+      <div className={styles.LinksContainer}>
+        <NavLinks router={router} />
+      </div>
+    </div>
+  );
+}
+
+function NavLinks({ router }) {
+  return (
+    <>
       <NavLink currentRoute={router.route} route="/">
         Home
       </NavLink>
@@ -15,8 +35,29 @@ export default function Nav() {
         Next
       </NavLink>
       <NavLink currentRoute={router.route} route="/projects">
-        Projects
+        Works
       </NavLink>
+      <NavLink currentRoute={router.route} route="/blog">
+        Blog
+      </NavLink>
+    </>
+  );
+}
+
+function HamburgerMenuButton({ onClick }) {
+  return (
+    <button onClick={onClick} className={styles.hamburgerMenuButton}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </button>
+  );
+}
+
+function HamburgerMenu({ router }) {
+  return (
+    <div className={styles.hamburgerMenu}>
+      <NavLinks router={router} />
     </div>
   );
 }
