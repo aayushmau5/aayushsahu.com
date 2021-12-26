96c02dd0a098e02368b61e7ffad57f6ad442651f
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-12-26 15:59:48 +0530

    fix HiddenExpand styles

--- components/MDX/HiddenExpand.tsx
+++ components/MDX/HiddenExpand.tsx
@@ -1,23 +1,9 @@
-import { useState } from "react";
-
 import styles from "@/styles/MdxComponentStyles/HiddenExpand.module.css";
 
 export default function HiddenExpand({ summary, children }) {
-  const [isOpen, setOpen] = useState(false);
-
   return (
     <details className={styles.details}>
-      <summary
-        className={styles.summary}
-        style={{
-          borderRadius: `0.55rem 0.55rem ${isOpen ? 0 : "0.55rem"} ${
-            isOpen ? 0 : "0.55rem"
-          }`,
-        }}
-        onClick={() => setOpen((v) => !v)}
-      >
-        {summary}
-      </summary>
+      <summary className={styles.summary}>{summary}</summary>
       <div className={styles.children}>{children}</div>
     </details>
   );
