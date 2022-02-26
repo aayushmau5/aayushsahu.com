f29f456af1ce974832d096790013c0b14f55ece6
Aayush Kumar Sahu <aayushmau5@gmail.com>
2022-02-26 10:02:28 +0530

    new code block styling

--- components/MDX/Pre/index.tsx
+++ components/MDX/Pre/index.tsx
@@ -1,15 +1,8 @@
 import styles from "./style.module.css";
 
-export default function Pre({ languageType = "", ...props }) {
+export default function Pre({ ...props }) {
   return (
-    <div
-      className={`${styles.container} ${
-        languageType ? styles.marginTop48 : ""
-      }`}
-    >
-      {languageType ? (
-        <div className={styles.languageTypeContainer}>{languageType}</div>
-      ) : null}
+    <div className={styles.container}>
       <pre
         {...props}
         className={`${props.className ? props.className : ""} ${styles.pre}`}
