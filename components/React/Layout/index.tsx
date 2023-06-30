d56f9efc5b1973ce362146ab47690959de861796
Aayush Kumar Sahu <aayushmau5@gmail.com>
2023-06-30 23:43:04 +0530

    fix spotify component on chrome

--- components/React/Layout/index.tsx
+++ components/React/Layout/index.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+
 import styles from "./style.module.css";
 import Footer from "./Footer";
 import Nav from "./Nav";
 
 export default function Layout({ children }) {
+  useEffect(() => {
+    console.log(
+      "%c Hi there!, I see you :)",
+      "font-weight: bold; font-size: 50px;color: white; text-shadow: 3px 3px 0 #4a5ae9;"
+    );
+  }, []);
+
   return (
     <>
       <Nav />
